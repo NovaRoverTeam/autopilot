192fb71a451a0774423e20fcbfa1108d69e5b461
Ben Steer <amstu2@student.monash.edu>
2018-05-05 19:35:15 +1000

    changed to float64s

--- src/autopilot.cpp
+++ src/autopilot.cpp
@@ -51,6 +51,7 @@ float MAX_SPD; // What percentage of max speed to do in auto mode?
 int msg_cnt; // Counter for message printing
 
 bool disable_lidar; // ROS parameter
+bool glen_enabled = 1;
 
 latlng retrieval_dest; // Retrieval destination
 
@@ -128,6 +129,7 @@ bool Start_Auto(autopilot::calc_route::Request  &req,
 {   
     // Possible states are STANDBY, TRAVERSE, AVOID, SEARCH
   n->setParam("/AUTO_STATE", "STANDBY"); // Auto starts in STANDBY
+  bool success;
 
   ROS_INFO_STREAM("\nAttempting to start autopilot sequence.");
 
@@ -149,33 +151,54 @@ bool Start_Auto(autopilot::calc_route::Request  &req,
   ros::WallTime start_, end_; // Measure Glen execution time
   
   // Call the service Calc_Route, measuring time
-  start_ = ros::WallTime::now();
-  bool success = auto_client.call(srv);
-  end_ = ros::WallTime::now();
-
-  if (success)
+  if(glen_enabled)
   {
-    ROS_INFO("\n-- Successfully retrieved waypoint route."); 
+    start_ = ros::WallTime::now();
+    success = auto_client.call(srv);
+    end_ = ros::WallTime::now();
 
-    double exe_time = (end_ - start_).toSec();
-    ROS_INFO_STREAM("Glen execution time (s): " << exe_time << ".");
+    if (success)
+    {
+      ROS_INFO("\n-- Successfully retrieved waypoint route."); 
+
+      double exe_time = (end_ - start_).toSec();
+      ROS_INFO_STREAM("Glen execution time (s): " << exe_time << ".");
+    }
+    else 
+      ROS_INFO("\n-- Failed to retrieve waypoint route."); 
+
+    vector<gps::Gps> gps_route = srv.response.route;
+    n_wps = route.size();
+
+    // Populate the latlng route list from the Gps route list
+    for (int i=0; i<n_wps; i++)
+    {
+      gps::Gps gps_coord = gps_route[i];
+
+      latlng ll_coord;
+      ll_coord.latitude = gps_coord.latitude;
+      ll_coord.longitude = gps_coord.longitude;
+
+      route.push_back(ll_coord);
+    }
   }
-  else 
-    ROS_INFO("\n-- Failed to retrieve waypoint route."); 
-
-  vector<gps::Gps> gps_route = srv.response.route;
-  n_wps = route.size();
-
-  // Populate the latlng route list from the Gps route list
-  for (int i=0; i<n_wps; i++)
+  else
   {
-    gps::Gps gps_coord = gps_route[i];
+    latlng coord1;
+    coord1.latitude   = rover_pos.latitude;
+    coord1.longitude  = rover_pos.longitude;
+    latlng coord2;
+    coord2.latitude   = req.destination.latitude;
+    coord2.longitude  = req.destination.longitude;
+    for (int j=20; j<=100; j=j+20)
+    {
+      latlng ll_coord;
+      ll_coord.latitude = coord1.latitude + (coord2.latitude - coord1.latitude) * (j/100);
+      ll_coord.longitude = coord1.longitude + (coord2.longitude - coord2.longitude) * (j/100);
+      route.push_back(ll_coord);
 
-    latlng ll_coord;
-    ll_coord.latitude = gps_coord.latitude;
-    ll_coord.longitude = gps_coord.longitude;
-
-    route.push_back(ll_coord);
+      success = 1;
+    }
   }
 
   n->setParam("/AUTO_STATE", "TRAVERSE"); // Start moving along route
